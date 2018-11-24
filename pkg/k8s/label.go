e4293449edb06098bb704cf5c2c4cd5e66e66ebb
Benjamin Borbe <bborbe@rocketnews.de>
2018-11-24 19:59:42 +0100

    add license

--- pkg/k8s/label.go
+++ pkg/k8s/label.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Benjamin Borbe All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package k8s
 
 type Labels map[string]string
