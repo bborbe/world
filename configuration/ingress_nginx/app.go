f98d84cf1fa3775c48a930fa0b98198754f444a1
Benjamin Borbe <bborbe@rocketnews.de>
2020-04-06 19:48:31 +0200

    fmt

--- configuration/ingress_nginx/app.go
+++ configuration/ingress_nginx/app.go
@@ -6,8 +6,8 @@ package ingress_nginx
 
 import (
 	"context"
-	"github.com/bborbe/world/pkg/docker"
 
+	"github.com/bborbe/world/pkg/docker"
 	"github.com/bborbe/world/pkg/k8s"
 	"github.com/bborbe/world/pkg/validation"
 	"github.com/bborbe/world/pkg/world"
