3687ae20c627f86ce7e35aa9bbad22318daa8b61
Benjamin Borbe <bborbe@rocketnews.de>
2021-01-21 11:09:47 +0100

    update jira

--- configuration/world.go
+++ configuration/world.go
@@ -621,7 +621,7 @@ func (w *World) netcup() map[AppName]world.Configuration {
 			Domains: k8s.IngressHosts{
 				k8s.IngressHost(JiraHostname),
 			},
-			Version:          "8.14.0",
+			Version:          "8.14.1",
 			DatabasePassword: w.TeamvaultSecrets.Password("eOB12w"),
 			SmtpUsername:     w.TeamvaultSecrets.Username("MwmE0w"),
 			SmtpPassword:     w.TeamvaultSecrets.Password("MwmE0w"),
