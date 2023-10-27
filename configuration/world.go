f3c3d4eecf7faf370d81dab28dff9cfb6daad6ac
Benjamin Borbe <bborbe@rocketnews.de>
2023-10-27 22:06:24 +0200

    update poste

--- configuration/world.go
+++ configuration/world.go
@@ -185,7 +185,7 @@ func (w *World) hetzner1() map[AppName]world.Configuration {
 		},
 		"poste": &service.Poste{
 			SSH:          ssh,
-			PosteVersion: "2.3.13", // https://hub.docker.com/r/analogic/poste.io/tags
+			PosteVersion: "2.3.14", // https://hub.docker.com/r/analogic/poste.io/tags
 			Port:         network.PortStatic(8001),
 		},
 		"webdav": &service.Webdav{
